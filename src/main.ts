fe0c5020ce56238e0ce5eb52a1c291b0393daab3
Juraj Simon <simon@shoptet.cz>
2024-03-01 19:02:04 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import * as core from '@actions/core'
-import { Eta } from 'eta'
-import YAML from 'yaml'
+import * as core from '@actions/core';
+import { Eta } from 'eta';
+import YAML from 'yaml';
+import dotenv from 'dotenv';
 
 /**
  * The main function for the action.
@@ -8,25 +9,55 @@ import YAML from 'yaml'
  */
 export async function run(): Promise<void> {
   try {
-    const template = core.getInput('template', { required: true })
+    const template = core.getInput('template', { required: true });
+    const varName = core.getInput('var_name', { required: true }) || 'it';
     const variables = core.getInput('variables', {
       required: false
-    })
+    });
 
-    console.log(`Template: ${template}`)
+    core.debug(`Template string: ${template}`);
+    core.debug(`Variables prefix in template: ${varName}`);
 
-    console.log('env', JSON.stringify(process.env, undefined, 2))
+    let parsedVariables: { [key: string]: unknown } | undefined;
 
-    const parsedVariables = YAML.parse(variables)
+    if (variables.length > 0) {
+      try {
+        parsedVariables = JSON.parse(variables);
+        console.debug('Variables parsed as JSON');
+      } catch {
+        // If the JSON parse fails, try to parse as YAML
+      }
 
-    console.log('variables', parsedVariables)
+      if (typeof parsedVariables !== 'object') {
+        try {
+          parsedVariables = YAML.parse(variables);
+          console.debug('Variables parsed as YAML');
+        } catch {
+          // If the YAML parse fails, try parse detoenv
+        }
+      }
 
-    const eta = new Eta()
-    const renderedTemplate = eta.renderString(template, { ...parsedVariables })
+      if (typeof parsedVariables !== 'object') {
+        try {
+          parsedVariables = dotenv.parse(Buffer.from(variables, 'utf8'));
+          console.debug('Variables parsed as dotenv');
+        } catch {
+          // If the dotenv parse fails, log an error
+        }
+      }
 
-    core.setOutput('text', renderedTemplate)
+      if (typeof parsedVariables !== 'object') {
+        console.log('Unable ot parse variables as JSON or YAML');
+      }
+
+      console.log('variables', parsedVariables);
+    }
+    const eta = new Eta({ varName });
+    const renderedTemplate = eta.renderString(template, { ...parsedVariables });
+
+    core.setOutput('text', renderedTemplate);
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
